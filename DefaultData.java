b44eafe553db86c4f68ccbb4b0011572190f70ac
fffd <90536245+fffdgod@users.noreply.github.com>
2025-10-22 22:48:16 +0800

    Add files via upload

--- DefaultData.java
+++ DefaultData.java
@@ -1,11 +1,11 @@
 
-public class DefaultPlayerVehicle {
+class DefaultPlayerVehicle {
     public static float hp= 50;
     public static float pkArmor=400;
     public static float ekArmor=400;
     public static float antiScout=15;
 } 
-public class DefaultVehicle {
+class DefaultVehicle {
     public static float hp= 25;
     public static float pkArmor=200;
     public static float ekArmor=200;
@@ -14,31 +14,31 @@ public class DefaultVehicle {
     
 
 
-public class DefaultTurrent{
+class DefaultTurrent{
     public static float hp = 15f;
     public static float antiScout = 15f;
 }
-public class DefaultSideTurrent{
-    public static float hp = 15f;
-    public static float antiScout = 15f;
-    public static float value = 1f;
-}
-public class DefaultAPS{
+class DefaultSideTurrent{
     public static float hp = 15f;
     public static float antiScout = 15f;
     public static float value = 1f;
 }
-public class DefaultExtraArmor{
+class DefaultAPS{
     public static float hp = 15f;
     public static float antiScout = 15f;
     public static float value = 1f;
 }
-public class DefaultScout{
+class DefaultExtraArmor{
     public static float hp = 15f;
     public static float antiScout = 15f;
     public static float value = 1f;
 }
-public class DefaultReloader{
+class DefaultScout{
+    public static float hp = 15f;
+    public static float antiScout = 15f;
+    public static float value = 1f;
+}
+class DefaultReloader{
     public static float hp = 15f;
     public static float antiScout = 15f;
     public static float value = 1f;
@@ -46,25 +46,24 @@ public class DefaultReloader{
 
 
 
-public class DefaultAP {
+class DefaultAP {
     public static float pd = 400; // 穿甲深度
     public static float ed = 200; // 破甲深度
     public static float damage = 10;
 }
 
-public class DefaultHEAT {
+class DefaultHEAT {
     public static float pd = 300; // 穿甲深度
     public static float ed = 300; // 破甲深度
     public static float damage = 15;
 }
 
-public class DefaultAPFSDS {
+class DefaultAPFSDS {
     public static float pd = 600; // 穿甲深度
     public static float ed = 0;   // 破甲深度
     public static float damage = 6;
 }
-
-public class DefaultMissile {
+class DefaultMissile {
     public static float pd = 600; // 穿甲深度
     public static float ed = 600; // 破甲深度
     public static float damage = 18;
